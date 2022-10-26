a208e1d6ac1d8ece1ff1cd48df719529f28e7934
Patrick Aloueichek <p.aloueichek@gmail.com>
2022-10-26 18:01:57 +0200

    initial commit

---
+++ beacon_ios/beacon_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beacon_ios
+//
+//  Created by Patrick Aloueichek on 26/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ beacon_ios/beacon_iosUITests/beacon_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  beacon_iosUITestsLaunchTests.swift
+//  beacon_iosUITests
+//
+//  Created by Patrick Aloueichek on 26/10/2022.
+//
+
+import XCTest
+
+final class beacon_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
